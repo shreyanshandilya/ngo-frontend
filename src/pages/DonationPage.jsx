eb06563377d96a8106f9407730b8772c4039da59
divyansh351 <divyanshgupta351@gmail.com>
2023-12-11 01:46:59 +0530

    multi image upload works fine now

--- src/pages/DonationPage.jsx
+++ src/pages/DonationPage.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
 import './Signup.css'
 import Navbar from '../components/NavbarLogged';
+import axios from "axios";
+import { SignalCellularNull } from "@mui/icons-material";
 
 const DonationPage = () => {
 
@@ -11,7 +13,8 @@ const DonationPage = () => {
     const [product_area_of_donation, setArea] = useState('');
     const [loading, setLoading] = useState(false);
     const [Error, setError] = useState(false);
-    const [image, setImage] = useState({})
+    const [Success, setSuccess] = useState(false)
+    const [image, setImage] = useState([])
 
     const [donor, setDonor] = useState({});
     const token = localStorage.getItem("token")
@@ -30,30 +33,39 @@ const DonationPage = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         setLoading(1);
+        setSuccess(0);
+        setError(0);
         let data = {
             product_title, product_category, product_description_before, product_defects_before, product_area_of_donation,
-            donor_mob_number: donor["donor"]["donor_mob_number"], image
+            donor_mob_number: donor["donor"]["donor_mob_number"]
         }
         const formData = new FormData();
-        for(let prop in data) {
+        for (let prop in data) {
             formData.append(prop, data[prop]);
         }
-        await fetch(`${process.env.REACT_APP_BASE_URL}/product`, {
-            method: "POST",
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
+        for (const img of image) { // images is an array of File Object
+            formData.append('image', img, img.name); // multiple upload
+        }
+        const response = await axios.post(
+            `${process.env.REACT_APP_BASE_URL}/product`,
+            formData,
+            {
+                headers: {
+                    'Content-Type': `multipart/form-data`,
+                },
             }
-        })
-            .then(response => response.json())
-            .then(json => {
-                console.log(formData)
-                console.log(json)
-            });
+        )
+        if (response.data.message == 'Donation Success') {
+            setLoading(0);
+            setSuccess(1);
+        }
+        else {
+            setError(1);
+        }
     }
 
     const handleImageChange = (e) => {
-        setImage(e.target.files[0]);
+        setImage(e.target.files);
     }
 
     return (<>
@@ -113,12 +125,14 @@ const DonationPage = () => {
                         <input
                             type="file"
                             onChange={handleImageChange}
+                            multiple
                         />
                     </div>
                     <button type="submit" onClick={handleSubmit}>
                         Submit
                     </button>
-                    {loading ? <div>Donation Succesful</div> : <></>}
+                    {loading ? <div>Donating</div> : <></>}
+                    {Success ? <div>Succefully Donated</div> : <></>}
                     {Error ? <div>Some error occured</div> : <></>}
                     <br></br>
                 </form>
